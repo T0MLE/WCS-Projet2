a7c8f397c8df00f7b58f1bbf3f5ca568a095cece
Kader B <kder000@gmail.com>
2023-04-17 19:19:22 +0200

    create component Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
-import Home from "./pages/Home";
-import Header from "./components/Header"
+import Header from "./components/Header";
 import "./App.css";
-import "./AppMobile.css"
-
+import "./AppMobile.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      
     </div>
   );
 }
