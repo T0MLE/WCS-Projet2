c58e8fe7a74557abc93a54da6734f007034f837c
T0MLE <lelaurain.tom@gmail.com>
2023-04-19 11:09:28 +0200

    optimized routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,10 @@ import BodyPartExercises from "./components/BodyPartExercises";
 import MannequinWrapper from "./components/MannequinWrapper";
 
 function App() {
-  const [exercise, setExercise] = useState([]);
+  const [exercises, setExercises] = useState([]);
+  const [filteredExercises, setFilteredExercises] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [uniqueEx, setUniqueEx] = useState([]);
   const options = {
     method: "GET",
     headers: {
@@ -15,75 +18,41 @@ function App() {
       "X-RapidAPI-Host": "musclewiki.p.rapidapi.com",
     },
   };
+  const controller = new AbortController();
+
   useEffect(() => {
     fetch("https://musclewiki.p.rapidapi.com/exercises", options)
       .then((response) => response.json())
-      .then((response) => setExercise(response))
+      .then((response) => setExercises(response))
       .catch((err) => console.error(err));
+    return () => {
+      controller.abort();
+    };
   }, []);
+
+  useEffect(() => {
+    setFilteredExercises(
+      exercises.filter((exercise) => exercise.target.Primary?.includes(filter))
+    );
+  }, [exercises, filter]);
+
+  useEffect(() => {
+    setUniqueEx([
+      ...new Map(filteredExercises.map((v) => [v.exercise_name, v])).values(),
+    ]);
+  }, [filteredExercises]);
+
   return (
     <Routes>
       <Route path="/" element={<MannequinWrapper />} />
       <Route
-        path="/Abdominals"
-        element={<BodyPartExercises exercise={exercise} />}
-      />
-      <Route
-        path="/Chest"
-        element={<BodyPartExercises exercise={exercise} />}
-      />
-      <Route
-        path="/Biceps"
-        element={<BodyPartExercises exercise={exercise} />}
-      />
-      <Route
-        path="/Triceps"
-        element={<BodyPartExercises exercise={exercise} />}
-      />
-      <Route
-        path="/Shoulders"
-        element={<BodyPartExercises exercise={exercise} />}
-      />
-      <Route
-        path="/Obliques"
-        element={<BodyPartExercises exercise={exercise} />}
-      />
-      <Route
-        path="/Forearms"
-        element={<BodyPartExercises exercise={exercise} />}
-      />
-      <Route
-        path="/Traps"
-        element={<BodyPartExercises exercise={exercise} />}
-      />
-      <Route
-        path="/Quads"
-        element={<BodyPartExercises exercise={exercise} />}
-      />
-      <Route
-        path="/Calves"
-        element={<BodyPartExercises exercise={exercise} />}
-      />
-      <Route
-        path="/Hamstrings"
-        element={<BodyPartExercises exercise={exercise} />}
-      />
-      <Route
-        path="/Lowerback"
-        element={<BodyPartExercises exercise={exercise} />}
-      />
-      <Route
-        path="/Glutes"
-        element={<BodyPartExercises exercise={exercise} />}
-      />
-      <Route path="/Lats" element={<BodyPartExercises exercise={exercise} />} />
-      <Route
-        path="/Midback"
-        element={<BodyPartExercises exercise={exercise} />}
-      />
-      <Route
-        path="/Traps"
-        element={<BodyPartExercises exercise={exercise} />}
+        path="/:exercise"
+        element={
+          <BodyPartExercises
+            exercises={uniqueEx}
+            handleExerciseChange={setFilter}
+          />
+        }
       />
     </Routes>
   );
--- frontend/src/components/BodyPartExercises.jsx
+++ frontend/src/components/BodyPartExercises.jsx
@@ -1,70 +1,25 @@
-import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
-function BodyPartExercises({ exercise }) {
-  const [filteredArr, setFilteredArr] = useState([]);
-  const [uniqueEx, setUniqueEx] = useState([]);
-  const splitPath = window.location.pathname.slice(1).split("b").join(" b");
-  console.info(splitPath.includes("back"));
-
-  useEffect(() => {
-    console.warn(exercise);
-    if (exercise.length === 0) {
-      const options = {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key": import.meta.env.VITE_APP_KEY,
-          "X-RapidAPI-Host": "musclewiki.p.rapidapi.com",
-        },
-      };
-      fetch("https://musclewiki.p.rapidapi.com/exercises", options)
-        .then((response) => response.json())
-        .then((response) => {
-          setFilteredArr(
-            response.filter((ex) =>
-              ex.target?.Primary?.includes(
-                window.location.pathname.slice(1).includes("back")
-                  ? splitPath
-                  : window.location.pathname.slice(1)
-              )
-            )
-          );
-        })
-        .catch((err) => console.error(err));
-    } else {
-      setFilteredArr(
-        exercise.filter((ex) =>
-          ex.target?.Primary?.includes(
-            window.location.pathname.slice(1).includes("back")
-              ? splitPath
-              : window.location.pathname.slice(1)
-          )
-        )
-      );
-    }
-  }, []);
-
-  useEffect(() => {
-    setUniqueEx([
-      ...new Map(filteredArr.map((v) => [v.exercise_name, v])).values(),
-    ]);
-  }, [filteredArr]);
+function BodyPartExercises({ exercises, handleExerciseChange }) {
+  const { exercise } = useParams();
+  handleExerciseChange(exercise);
 
   return (
     <div>
       <button type="button">
         <Link to="/">RETOUR</Link>
       </button>
-      {uniqueEx.map((e) => {
+      {exercises?.map((e) => {
         return <p key={e.id}>{e.exercise_name}</p>;
       })}
     </div>
   );
 }
 
-BodyPartExercises.propTypes = {
-  exercise: PropTypes.arrayOf(PropTypes.shape).isRequired,
-};
-
 export default BodyPartExercises;
+
+BodyPartExercises.propTypes = {
+  exercises: PropTypes.arrayOf(PropTypes.shape).isRequired,
+  handleExerciseChange: PropTypes.func.isRequired,
+};
