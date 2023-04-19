856686e23b9b5f161685f834f20561b61299d7b2
Omci <melloulchi.omar@gmail.com>
2023-04-19 11:09:23 +0200

    Created Exercise component to for reuse in other sections

--- frontend/src/components/Exercise.jsx
+++ frontend/src/components/Exercise.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Exercise() {
+function Exercise({ name, video, description }) {
   return (
-    <>
-      <h2>Exercice</h2>
-      <h2>hello</h2>
-    </>
+    <div>
+      <p>{name}</p>
+      <video controls>
+        <source src={video} type="video/mp4" />{" "}
+        <track default kind="captions" />
+      </video>
+      <p>{description}</p>
+    </div>
   );
 }
+
+Exercise.propTypes = {
+  name: PropTypes.string.isRequired,
+  video: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
 export default Exercise;
