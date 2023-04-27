74c5195258cba8853b65d50bb4719307b0013125
T0MLE <lelaurain.tom@gmail.com>
2023-04-27 19:27:14 +0200

    styled searchbar

--- frontend/src/components/BodyPartExercises.jsx
+++ frontend/src/components/BodyPartExercises.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import Exercise from "./Exercise";
 
@@ -9,6 +9,7 @@ function BodyPartExercises({ exercises, handleExerciseChange }) {
   handleExerciseChange(exercise);
   if (exercise === "Lowerback") handleExerciseChange("Lower back");
   if (exercise === "Midback") handleExerciseChange("Mid back");
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -61,16 +62,27 @@ function BodyPartExercises({ exercises, handleExerciseChange }) {
 
         <h2>{exercise} exercises</h2>
       </div>
-      {exercises?.map((e) => {
-        return (
-          <Exercise
-            key={e.id}
-            name={e.exercise_name}
-            video={e.videoURL[0]}
-            description={e.steps}
-          />
-        );
-      })}
+      <input
+        className="searchbar"
+        type="text"
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+        placeholder="Search your exercise"
+      />
+      {exercises
+        ?.filter((item) =>
+          item.exercise_name.toLowerCase().includes(search.toLowerCase())
+        )
+        .map((e) => {
+          return (
+            <Exercise
+              key={e.id}
+              name={e.exercise_name}
+              video={e.videoURL[0]}
+              description={e.steps}
+            />
+          );
+        })}
     </div>
   );
 }
