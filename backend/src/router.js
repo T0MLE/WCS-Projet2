3b3ed76d8b116841e91c41586acc90a8fb624738
T0MLE <lelaurain.tom@gmail.com>
2023-05-26 17:27:17 +0200

    moved nutrition data to frontend

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,8 +4,6 @@ const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
 
-const data = require("./data.json");
-
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
@@ -13,9 +11,5 @@ router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
 
 // Route pour le fichier data.json
-router.get("/data", (req, res) => {
-  console.info(data);
-  res.json(data);
-});
 
 module.exports = router;
